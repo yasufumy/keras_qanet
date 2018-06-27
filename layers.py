65638312a1352ed379605885e41b6c068f8173d0
yasufumi <yasufumi.taniguchi@gmail.com>
2018-06-27 14:16:13 +0900

    add multiheadattention and positionembedding layers

---
+++ layers.py
@@ -0,0 +1,104 @@
+import math
+
+import tensorflow as tf
+from keras import backend as K
+from keras.engine.topology import Layer
+
+
+class PositionEmbedding(Layer):
+    def __init__(self, min_timescale=1., max_timescale=1.e4, **kwargs):
+        self.min_timescale = min_timescale
+        self.max_timescale = max_timescale
+        super().__init__(**kwargs)
+
+    def get_timing_signal_1d(self, length, channels):
+        position = tf.to_float(tf.range(length))
+        num_timescales = channels // 2
+        log_time_scale_increment = math.log(float(self.max_timescale) /
+                                            float(self.min_timescale)) / (tf.to_float(num_timescales) - 1)
+        inv_timescales = self.min_timescale * tf.exp(tf.to_float(tf.range(num_timescales)) * -log_time_scale_increment)
+        scaled_time = tf.expand_dims(position, 1) * tf.expand_dims(inv_timescales, 0)
+        signal = tf.concat([tf.sin(scaled_time), tf.icos(scaled_time)], axis=1)
+        signal = tf.pad(signal, [[0, 0], [0, tf.mod(channels, 2)]])
+        signal = tf.reshape(signal, [1, length, channels])
+        return signal
+
+    def add_timing_signal_1d(self, x):
+        length = tf.shape(x)[1]
+        channels = tf.shape(x)[2]
+        signal = self.get_timing_signal_1d(length, channels)
+        return x + signal
+
+    def call(self, x, mask=None):
+        return self.add_timing_signal_1d(x)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+class MultiHeadAttention(Layer):
+    def __init__(self, hidden_size, num_heads, dropout=0.0, bias=False, **kwargs):
+        self.hidden_size = hidden_size
+        self.num_heads = num_heads
+        self.dropout = dropout
+        self.bias = bias
+        super().__init__(**kwargs)
+
+    def build(self, input_shape):
+        if self.bias:
+            self.b = self.add_weight(
+                name='bias', shape=(input_shape[0][-2],), initializer='zero')
+
+        super().build(input_shape)
+
+    def split_last_dim(self, x, n):
+        old_shape = x.get_shape().dims
+        last = old_shape[-1]
+        new_shape = old_shape[:-1] + [n] + [last // n if last else None]
+        ret = tf.reshape(x, tf.concat([tf.shape(x)[:-1], [n, -1]], 0))
+        ret.set_shape(new_shape)
+        return tf.transpose(ret, [0, 2, 1, 3])
+
+    def mask_logits(self, inputs, mask, mask_value=tf.float32.min):
+        shapes = [x if x is not None else -1 for x in inputs.shape.as_list()]
+        mask = K.cast(mask, tf.int32)
+        mask = K.one_hot(mask[:, 0], shapes[-1])
+        mask = 1 - K.cumsum(mask, 1)
+        mask = tf.cat(mask, tf.float32)
+        mask = tf.reshape(mask, [shapes[0], 1, 1, shapes[-1]])
+        return inputs + mask_value * (1 - mask)
+
+    def dot_product_attention(self, x, seq_len=None, dropout=.1, training=None):
+        q, k, v = x
+        logits = tf.matmul(q, k, transpose_b=True)
+        if self.bias:
+            logits += self.b
+        if seq_len is not None:
+            logits = self.mask_logits(logits, seq_len)
+        weights = tf.nn.softmax(logits)
+        weights = K.in_train_phase(K.dropout(weights, dropout), weights, training=training)
+        return tf.matmul(weights, v)
+
+    def combine_last_two_dims(self, x):
+        old_shape = x.get_shape().dims
+        a, b = old_shape[-2:]
+        new_shape = old_shape[:-2] + [a * b if a and b else None]
+        ret = tf.reshape(x, tf.concat([tf.shape(x)[:-2], [-1]], 0))
+        ret.set_shape(new_shape)
+        return ret
+
+    def call(self, x, mask=None, training=None):
+        key_and_value, query, seq_len = x
+        Q = self.split_last_dim(query, self.num_heads)
+        key, value = tf.split(key_and_value, 2, axis=2)
+        K = self.split_last_dim(key, self.num_heads)
+        V = self.split_last_dim(value, self.num_heads)
+
+        scale = (self.hidden_size // self.num_heads) ** (1/2)
+        Q *= scale
+        x = self.dot_product_attention([Q, K, V], seq_len, dropout=self.dropout, training=training)
+        x = self.combine_last_two_dims(tf.transpose(x, [0, 2, 1, 3]))
+        return x
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[1]
