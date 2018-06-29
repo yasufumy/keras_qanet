91e4035d214690edeb4b829680a0ae3eb31442e8
yasufumi <yasufumi.taniguchi@gmail.com>
2018-06-29 04:58:59 +0000

    update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from trainer import SquadTrainer
 # from metrics import SquadMetric
 # from utils import evaluate
 
-
 parser = ArgumentParser()
 parser.add_argument('--epoch', default=100, type=int)
+parser.add_argument('--batch', default=64, type=int)
 args = parser.parse_args()
 
 
@@ -22,32 +22,29 @@ spacy_en = spacy.load('en_core_web_sm',
 
 def tokenizer(x): return [token.text.lower() for token in spacy_en(x) if not token.is_space]
 
+PAD_TOKEN = '<pad>'
+UNK_TOKEN = '<unk>'
+
 
 if not os.path.exists('vocab.pkl'):
-    squad_tokens = load_squad_tokens('./data/train-v2.0.txt')
+    squad_tokens = load_squad_tokens('./data/train-v1.1_filtered.txt', tokenizer)
     token_to_index, index_to_token = Vocabulary.build(
-        squad_tokens, 5, 3000, ('<pad>', '<unk>'), 'vocab.pkl')
+        squad_tokens, 5, 3000, (PAD_TOKEN, UNK_TOKEN), 'vocab.pkl')
 
 else:
     token_to_index, index_to_token = Vocabulary.load('vocab.pkl')
 
-batch_size = 256  # Batch size for training.
+batch_size = args.batch # Batch size for training.
 epochs = args.epoch  # Number of epochs to train for.
 latent_dim = 128  # Latent dimensionality of the encoding space.
 num_encoder_tokens = len(token_to_index)
-num_decoder_tokens = 3
-
 
 print('Number of unique input tokens:', num_encoder_tokens)
-print('Number of unique output tokens:', num_decoder_tokens)
 
-decoder_token_to_index = {'ignore': 0, 'start': 1, 'keep': 2}
-decoder_index_to_token = ['ignore', 'start', 'keep']
-
-model = LightQANet(len(token_to_index), latent_dim)
+model = LightQANet(len(token_to_index), latent_dim, latent_dim).build()
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
-dataset = SquadReader('data/train-v2.0.txt')
-converter = SquadConverter(token_to_index, 1, '<pad>', 3)
+dataset = SquadReader('./data/train-v1.1_filtered.txt')
+converter = SquadConverter(token_to_index, PAD_TOKEN, UNK_TOKEN)
 train_generator = Iterator(dataset, batch_size, converter)
 trainer = SquadTrainer(model, train_generator, epochs)
 trainer.run()
