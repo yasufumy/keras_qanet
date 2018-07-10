a86f2df47409020e978dae726d09b8626b046bc8
yasufumi <yasufumi.taniguchi@gmail.com>
2018-07-10 14:53:05 +0900

    add test case for utils.split_dataset in tests/test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from unittest.mock import MagicMock, Mock, patch, mock_open, call
 from utils import char_span_to_token_span, get_spans, evaluate, filter_dataset, \
-    make_small_dataset
+    make_small_dataset, split_dataset
 
 
 class TestUitls(TestCase):
@@ -72,4 +72,21 @@ class TestUitls(TestCase):
         open_.assert_has_calls([call(filename), call(dest_path, 'w')], any_order=True)
         writer.assert_called_once_with(open_.return_value, delimiter='\t')
         writer.return_value.writerow.assert_called_once()
+        patch.stopall()
+
+    def test_split_dataset(self):
+        filename = '/path/to/dataset.tsv'
+        train_filename = '/path/to/dataset_train.tsv'
+        dev_filename = '/path/to/dataset_dev.tsv'
+        read_data = 'Rock n Roll is a risk. You rick being ridiculed.\tDo you like rock music?\n' \
+            'Rock n Roll is a risk.\tDo you like rock?'
+        open_ = patch('utils.open', mock_open(read_data=read_data)).start()
+        open_.return_value.__iter__.return_value = read_data.split('\n')
+        writer = patch('csv.writer').start()
+        split_dataset(filename)
+        open_.assert_has_calls(
+            [call(filename), call(train_filename, 'w'), call(dev_filename, 'w')],
+            any_order=True)
+        writer.assert_called_with(open_.return_value, delimiter='\t')
+        writer.return_value.writerow.assert_called()
         patch.stopall()
