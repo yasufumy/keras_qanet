37cca18cbbf2fd5697ffa17f43b7f64eeddec897
yasufumi <yasufumi.taniguchi@gmail.com>
2018-07-20 14:13:11 +0900

    update TestQANet

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,14 +4,17 @@ from keras import backend as K
 from models import QANet
 
 
-class TestLightQANet(TestCase):
+class TestQANet(TestCase):
     def test_build(self):
         vocab_size = 3000
-        embed_size = filters = 32
+        embed_size = filters = 96
         context_limit = 40
         query_limit = 5
+        num_heads = 1
         model = QANet(
-            vocab_size, embed_size, filters, context_limit, query_limit).build()
+            vocab_size, embed_size, filters, num_heads, context_limit, query_limit,
+            dropout=.1, encoder_layer_size=1, encoder_conv_blocks=3,
+            output_layer_size=7, output_conv_blocks=2).build()
         query_input, context_input = model.inputs
         start_prob, end_prob, S_bar, S_T = model.outputs
 
